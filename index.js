e98a0852d6c5d9d26ecc47f971d96186038df23f
jadamsuryateja <jadamsuryateja123456@gmail.com>
2025-11-06 12:35:56 +0530

    1

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://andhravikasam-fronend.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
